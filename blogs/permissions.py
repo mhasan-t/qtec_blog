185a0ee874f6e4e2aec07db93efdfb9ab4d19e2c
mhasan-t <tahnoonn19@gmail.com>
2024-02-14 20:46:52 +0600

    Wrote Category, Blog and Post views and permissions.

---
+++ blogs/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+
+
+class IsAuthor(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.user_type == 2
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
-
 # Create your views here.
+from rest_framework import viewsets, permissions
+
+from .models import Category, Blog, Post
+from .permissions import IsAuthor, ReadOnly
+from .serializers import CategorySerializer, BlogSerializer, PostSerializer
+
+
+class CategoryViews(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class BlogViews(viewsets.ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    permission_classes = [permissions.IsAdminUser | IsAuthor | ReadOnly]
+
+
+class PostViews(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAdminUser | IsAuthor | ReadOnly]
