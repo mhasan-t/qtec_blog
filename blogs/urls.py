60c13a602a624b18c41a10eea0eef938111b30dd
mhasan-t <tahnoonn19@gmail.com>
2024-02-14 20:48:12 +0600

    Wrote Category, Blog and Post urls.

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,2 +1,10 @@
-urlpatterns = [
-]
+from rest_framework import routers
+
+from . import views
+
+router = routers.SimpleRouter()
+router.register(r'categories', views.CategoryViews)
+router.register(r'blogs', views.BlogViews)
+router.register(r'posts', views.PostViews)
+
+urlpatterns = router.urls
