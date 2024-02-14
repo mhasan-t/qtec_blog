d536e16b31e5c37c511bc4e9ed1737607d808e4d
mhasan-t <tahnoonn19@gmail.com>
2024-02-14 20:35:45 +0600

    Wrote Category, Blog and Post models.

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+
+class Blog(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='blogs')
+    title = models.CharField(max_length=150)
+    banner = models.ImageField()
+    details = models.TextField()
+    total_views = models.IntegerField(default=0)
+
+    REQUIRED_FIELDS = ['category', 'title', 'banner', 'details']
+
+    def __str__(self) -> str:
+        return f'{self.title} - ({self.category})'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    post_text = models.TextField()
+    blog = models.ForeignKey(Blog, on_delete=models.SET_NULL, related_name='posts')
