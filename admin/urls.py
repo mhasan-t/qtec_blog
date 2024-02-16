12285993d39ad49de88f34d449d4e3f97ab4eb62
mhasan-t <tahnoonn19@gmail.com>
2024-02-16 15:13:33 +0600

    Admin blogs page.

--- admin/urls.py
+++ admin/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import AdminDashboardView, AdminDashboardChartData, AdminBlogListView
+from .views import AdminDashboardView, AdminDashboardChartData, AdminBlogListView, AdminBlogDeleteView
 
 app_name = 'admin'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', AdminDashboardView.as_view(), name='dashboard'),
     path('dashboard_data', AdminDashboardChartData.as_view(), name='dashboard-data'),
     path('blogs/', AdminBlogListView.as_view(), name='blogs'),
+    path('blogs/<int:pk>/delete', AdminBlogDeleteView.as_view(), name='blog-delete'),
 ]
--- admin/views.py
+++ admin/views.py
@@ -1,9 +1,12 @@
 from http import HTTPStatus
 
+from django.core.paginator import Paginator
 from django.db.models import Count
-from django.http.response import JsonResponse, HttpResponse
+from django.http.response import JsonResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic import DeleteView
 
 from blogs.models import Blog, Category, Post
 
@@ -33,4 +36,34 @@ class AdminDashboardChartData(View):
 
 class AdminBlogListView(View):
     def get(self, request):
-        return HttpResponse("blogs")
+        blogs = Blog.objects.all()
+        categories = Category.objects.all()
+
+        # Filter by category
+        category = request.GET.get("category")
+        if category:
+            category_instance = Category.objects.get(title=category)
+            blogs = blogs.filter(category=category_instance)
+
+        # Filter by author name
+        author = request.GET.get("author")
+        if author:
+            print(author)
+            blogs = blogs.filter(creator__full_name__icontains=author)
+
+        # Pagination
+        paginator = Paginator(blogs, 5)
+        page_number = request.GET.get("page")
+        blogs_page = paginator.get_page(page_number)
+
+        context = {
+            'blogs': blogs_page,
+            'categories': categories
+        }
+        return render(request, "blogs.html", context)
+
+
+class AdminBlogDeleteView(DeleteView):
+    model = Blog
+    template_name = 'blog_confirm_delete.html'  # Template for confirmation page
+    success_url = reverse_lazy('admin:dashboard')
